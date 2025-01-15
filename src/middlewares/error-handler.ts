1dd12b819610fb0ed4848e67d72f97cc2f791b92
Woszuk <hejnaradrian@gmail.com>
2025-01-15 15:23:23 +0100

    Add error handler and use middlewares

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import Joi from "joi";
+import { NotFoundError } from "src/errors/NotFoundError";
+import { ValidationError } from "src/errors/ValidationError";
+
+const mapJoiError = (error: Joi.ValidationError) => {
+  return error.details.map(err => {
+    return { path: err.path[0], message: err.message.split(" ").slice(1).join(" ") };
+  });
+};
+
+export const errorHandler = (err: Error, _1: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ValidationError) {
+    res.status(400).send({
+      message: err.message,
+      details: mapJoiError(err.details),
+    });
+  } else if (err instanceof NotFoundError) {
+    res.status(404).send({
+      status: "Not Found error",
+      message: err.message,
+    });
+  } else {
+    res.status(500).send("Internal server error");
+  }
+  next();
+};
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { errorHandler } from "src/middlewares/error-handler";
+import { notFoundHandler } from "src/middlewares/not-found-handler";
 import { productsRouter } from "src/routes/products";
 
 export const createServer = () => {
@@ -8,6 +10,10 @@ export const createServer = () => {
   server.use(express.json());
 
   server.use("/products", productsRouter(router));
+
+  server.use(notFoundHandler);
+
+  server.use(errorHandler);
 
   return server;
 };
